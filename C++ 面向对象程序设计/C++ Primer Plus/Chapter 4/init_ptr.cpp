9143ef7b94196145ed4cfe0a8aba954f279430a7
CCP101 <liwentao0523@hotmail.com>
2018-03-31 12:57:17 +0800

    unkown change

--- C++ 面向对象程序设计/C++ Primer Plus/Chapter 4/init_ptr.cpp
+++ C++ 面向对象程序设计/C++ Primer Plus/Chapter 4/init_ptr.cpp
@@ -6,8 +6,8 @@ int main()
     int higgens = 5;
     int * pt = &higgens;
 
-    cout << "Value of higgens = " << higgens
-         << "; Address of higgens = " << &higgens << endl;
+	cout << "Value of higgens = " << higgens
+		<< "; Address of higgens = " << &higgens << endl;
     cout << "Value of *pt = " << *pt
          << "; Value of pt = " << pt << endl;
     // cin.get();
