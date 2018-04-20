88d07681dceaf917650a3717f702fc29dabda293
CCP101 <liwentao0523@hotmail.com>
2018-04-20 11:50:57 +0800

    Create 4-2.cpp

---
+++ C++ 面向对象程序设计/C++ study/C++上机/4-2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class triangle
+{
+    public:
+    triangle()
+    {
+        cin>>a>>b>>c;
+    }
+    int triangle_area()
+    {
+        p=(a+b+c)/2;
+        area=sqrt((p*(p-a)*(p-b)*(p-c)));
+        return area;
+    }
+    int triangle_length()
+    {
+        length=a+b+c;
+        return length;
+    }
+    void print()
+    {
+        cout<<"a="<<a<<"  b="<<b<<"  c="<<c<<endl;
+        cout<<"area="<<area<<endl<<"length="<<length<<endl;
+    }
+    private:
+    int a,b,c;
+    int area,length;
+    int p;
+};
+int main()
+{
+    triangle z;
+    z.triangle_area();
+    z.triangle_length();
+    z.print();
+    return 0;
+}
