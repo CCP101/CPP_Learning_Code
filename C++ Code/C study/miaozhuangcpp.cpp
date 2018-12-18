164a59037d37d4a66fa6b869bf8350942e60bb22
ccp101 <liwentao0523@hotmail.com>
2018-12-18 18:45:29 +0800

    C++ Primer Learning
    start learning chapter 1
    To page 1
    
    Time:2018/12/18
    Location:dormitory

---
+++ C++ Code/C study/miaozhuangcpp.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[5] = { 1,10,15,100 };
+	int *q = a;
+	int x;
+	cin >> x;
+	int *address = &x;
+	if (x > a[3])
+		a[4] = x;
+	for (int i = 0; i <= 3; i++)
+	{
+		if (x<*q)
+		{
+			int *p = q;
+			if (*(q+2)!=NULL)
+			{
+				*(q + 1) = *q;
+				q++;
+			}
+			*p = x;
+			break;
+		}
+		q++;
+	}
+	q = a;//再次地址变为a[0]
+	for (int i = 0; i <= 4; i++)
+		printf("%4d", *(q + i));
+	getchar();
+}
