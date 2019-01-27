2b1a07c96f963eb07a712262eda6d86f7173504f
ccp101 <liwentao0523@hotmail.com>
2019-01-27 22:22:02 +0800

    delete temp file

---
+++ C++ Code/C study/miaozhuang_color_print.cpp
@@ -0,0 +1,64 @@
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("\033[40;31m *你要改变颜色的内容，黑底和红色字* \033[0m \n");
+//	printf("\033[47;32m *你要改变颜色的内容，白底和绿色字* \033[0m \n");
+//	printf("\033[43;35m *你要改变颜色的内容，黄底和紫色字* \033[0m \n");
+//	printf("[0;31m *你要改变颜色的内容* [0;0m\n");
+//}
+
+
+
+//#include<stdio.h>
+//#define line 14
+//void square(void);//打印一行方框
+//
+//int main()
+//{
+//	printf("\033[40;31m*你要改变颜色的内容，黑底和红色字*\033[0m\n");
+//
+//
+//}
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+	
+		printf("************************* \n");
+		printf("\033[0;30;41m color!!! \033[0m Hello \n");
+		printf("\033[1;30;41m color!!! \033[0m Hello \n");
+		printf("\033[4;30;41m color!!! \033[0m Hello \n");
+		printf("\033[5;30;41m color!!! \033[0m Hello \n");
+		printf("\033[7;30;41m color!!! \033[0m Hello \n");
+		printf("\033[8;30;41m color!!! \033[0m Hello \n");
+
+
+		printf("************************* \n");
+		printf("\033[0;30;41m color!!! \033[0m Hello \n");
+		printf("\033[0;31;41m color!!! \033[0m Hello \n");
+		printf("\033[0;32;41m color!!! \033[0m Hello \n");
+		printf("\033[0;33;41m color!!! \033[0m Hello \n");
+		printf("\033[0;34;41m color!!! \033[0m Hello \n");
+		printf("\033[0;35;41m color!!! \033[0m Hello \n");
+		printf("\033[0;36;41m color!!! \033[0m Hello \n");
+		printf("\033[0;37;41m color!!! \033[0m Hello \n");
+
+		printf("************************* \n");
+		printf("\033[0;30;40m color!!! \033[0m Hello \n");
+		printf("\033[0;30;41m color!!! \033[0m Hello \n");
+		printf("\033[0;30;42m color!!! \033[0m Hello \n");
+		printf("\033[0;30;43m color!!! \033[0m Hello \n");
+		printf("\033[0;30;44m color!!! \033[0m Hello \n");
+		printf("\033[0;30;45m color!!! \033[0m Hello \n");
+		printf("\033[0;30;46m color!!! \033[0m Hello \n");
+		printf("\033[0;30;47m color!!! \033[0m Hello \n");
+
+		int njum;
+		cin >> njum;
+
+		return 0;
+}
