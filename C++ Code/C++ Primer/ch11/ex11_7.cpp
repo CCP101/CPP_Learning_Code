0e3524d39c1f084f0761490ff0523391ea04fd68
ccp101 <liwentao0523@hotmail.com>
2019-02-04 22:17:27 +0800

    2018 chinese new year temp change plan

--- C++ Code/C++ Primer/ch11/ex11_7.cpp
+++ C++ Code/C++ Primer/ch11/ex11_7.cpp
@@ -1,11 +1,3 @@
-// Yue Wang Oct, 2015
-//
-// Exercise 11.7:
-// Define a map for which the key is the family’s last name and
-// the value is a vector of the children’s names. Write code to
-// add new families and to add new children to an existing family.
-//
-
 #include <iostream>
 #include <map>
 #include <string>
